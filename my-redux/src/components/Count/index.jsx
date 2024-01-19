a79fa446a9d47239ce09bdf0bb46fc55eeab4008
春夏秋冬在中南 <2541093882@qq.com>
2024-01-19 23:19:13 +0800

    react学习

---
+++ my-redux/src/components/Count/index.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+/**
+ * 这是一个UI组件，不能有redux相关的东西
+ * 例如，不能导入 import {..., ...} from './../redux/count_action'
+ */
+export default class Count extends React.Component {
+
+  increment = () => {
+    // 获取选择的数值
+    const {value} = this.selectNumber;
+    this.props.add(value * 1);
+
+    /** 👇🏻下面这行也应该删除，不能看见任何redux的东西 */
+    // store.dispatch(createIncrementAction(value));
+  }
+
+  decrement = () => {
+    // 获取选择的数值
+    const {value} = this.selectNumber;
+    this.props.minus(value * 1);
+  }
+
+  incrementIfOdd = () => {
+    const {value} = this.selectNumber;
+    if (this.props.count % 2 !== 0) {
+      this.props.add(value * 1);
+    }
+  }
+
+  // 异步加
+  incrementAsync = () => {
+    const {value} = this.selectNumber;
+    this.props.addAsync(value * 1, 500);
+  }
+
+  render() {
+    // console.log('UI组件接收到的props是：', this.props);
+    // 输出 {store: {...}, count: -1, add: ƒ, minus: ƒ, addAsync: ƒ}
+
+    return (
+      <div>
+        <h1>当前求和为: {this.props.count}</h1>
+        <select ref = {c => this.selectNumber = c}>
+          <option value = {1}>1</option>
+          <option value = {2}>2</option>
+          <option value = {3}>3</option>
+        </select>&nbsp;
+
+        <button onClick = {this.increment}>加1</button>
+        &nbsp;
+        <button onClick = {this.decrement}>减1</button>
+        &nbsp;
+        <button onClick = {this.incrementIfOdd}>当和是奇数再加1</button>
+        &nbsp;
+        <button onClick = {this.incrementAsync}>异步加1</button>
+      </div>
+    );
+  }
+}
---
+++ my-redux/src/redux/count_action.js
@@ -0,0 +1,18 @@
+import {DECREMENT, INCREMENT} from "./constants";
+
+/**
+ * 该文件专门为Count组件生成action对象
+ */
+
+// 同步action，返回一个Object。即，同步action，就是指action的类型为Object类型的一般对象。
+export const createIncrementAction = data => ({type: INCREMENT, data});
+
+export const createDecrementAction = data => ({type: DECREMENT, data});
+
+export const createIncrementAsyncAction = (data, time) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(createIncrementAction(data));
+    }, time);
+  }
+}
